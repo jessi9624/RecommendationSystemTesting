35a82c0f694195941d80d42f2ae6ee5ae0a28bed
jessi9624 <47840638+jessi9624@users.noreply.github.com>
2021-04-09 17:45:52 +0530

    Update tests.py

--- store/tests.py
+++ store/tests.py
@@ -1,12 +1,14 @@
 from django.test import TestCase
 from store.models.Customer_form import Contact1
 from store.models.category import Category
+from store.models.customer  import Customer
 
 # Create your tests here.
 class Basictest(TestCase):
     def setUp(self):
         self.blog=Contact1.objects.create(Relation='father',name='chandan',dob='2021-04-01',category='Earphones')
         self.blog1=Category.objects.create(name='Earphones')
+        self.sign=Customer.objects.create(first_name='chandan',last_name='kamal',phone='1234567891',dob='2021-03-01',email='test1@gmail.com',password='123456')
     
     def test_model(self):
         d=self.blog
@@ -17,5 +19,10 @@ class Basictest(TestCase):
         e=self.blog1
         self.assertTrue(isinstance(e,Category))
         self.assertEqual(str(e),'Earphones') 
+    def test_model2(self):
+        f=self.sign
+        self.assertTrue(isinstance(f,Customer))
+        self.assertEqual(str(f),'Customer object (1)')
      
+    
         
